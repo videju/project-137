e778b764a94df21cca166c758e5a36e767c7478f
videju <77975858+videju@users.noreply.github.com>
2022-11-11 06:37:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+objects = [];
+dstatus = ""
+video = ""
+function preload(){
+    video  = createVideo("video.mp4")
+    video.hide();
+
+  
+}
+function setup(){
+canvas = createCanvas(480,380)
+canvas.center()
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("input_id").innerHTML = "object detection: true"
+}
+function modelLoaded(){
+    video.loop();
+    dstatus = true;
+    video.volume(0)
+    video.speed(1)
+}
+function gotResults(error,results){
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(video,0,0,480,380)
+    if(dstatus != ""){
+        objectDetector.detect(video,gotResults);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = 'Status : Objects Detected'
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+            fill("#FF0000");  
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent  + "%", objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x, objects[i].y,objects[i].width, objects[i].height)
+        }
+    }
+}
